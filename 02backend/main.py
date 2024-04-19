05b6b3b8970b62f9ec3467cc2a5d13deba24bb7d
FelipeCabelloE <98831429+FelipeCabelloE@users.noreply.github.com>
2024-04-19 19:20:48 -0400

    integrate query with db

--- 02backend/main.py
+++ 02backend/main.py
@@ -2,6 +2,16 @@ from fastapi import FastAPI
 import pandas as pd
 import duckdb
 from fastapi.middleware.cors import CORSMiddleware
+from datetime import datetime
+
+import os
+# Get the current working directory
+cwd = os.getcwd()
+
+# Construct the desired file path
+file_path = os.path.join(cwd, '..', '01data','interim', 'mydb.db')
+
+
 
 app = FastAPI()
 
@@ -15,6 +25,38 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+def get_timeseries_subset(db_conn, variable, organization, start_time, end_time):
+    query = """
+        SELECT value, timestamp
+        FROM timeseries_dataset
+        WHERE variable = ?
+          AND organization = ?
+          AND timestamp BETWEEN ? AND ?
+        ORDER BY timestamp;
+    """
+    
+    result = db_conn.execute(query, [variable, 
+                                     organization, 
+                                     start_time, 
+                                     end_time]).fetchdf()
+    return result
+
+
+
+
 @app.get('/')
 def read_root():
-    return {'ping':'pong'}
+    db = duckdb.connect()
+
+    db.execute(f"IMPORT DATABASE '{file_path}'")
+    df = get_timeseries_subset(db, 'CHL-01', 'adasa', datetime(2023, 12, 1),datetime(2024, 12, 1))
+    df = df.dropna()
+
+    db.close()
+    return {'data':df['timestamp'].values.tolist()}
+
+
+@app.get('/edt')
+def notas():
+    return {'promedio de notas':'7.0'}
